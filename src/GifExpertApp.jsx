01b1ba96e263e3d1defb8fb47fae1097360a97a2
htmtmx <cesar.pineda@daksha.mx>
2024-06-20 11:10:46 -0600

    🎉 Initial commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+      {/* Titulo de la app */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input (componente independiente) */}
+
+      {/* Listado de gifs */}
+        {/* GifItem */}
+    </>
+  )
+}
