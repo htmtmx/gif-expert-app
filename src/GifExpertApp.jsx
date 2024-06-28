8a29d4c824c4571a8c560f927078abbd31847034
htmtmx <cesar.pineda@daksha.mx>
2024-06-28 17:39:16 -0600

    🚧 Add initial categories to GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Demon Slayer']);
+  
   return (
     <>
       {/* Titulo de la app */}
       <h1>GifExpertApp</h1>
 
+      <ol>
+        <li>123</li>
+        <li>abc</li>
+        <li>xyz</li>
+        {categories.map((categorie, index) => {
+          return (<li key={index}>{categorie}</li>);
+        })}
+      </ol>
       {/* Input (componente independiente) */}
 
       {/* Listado de gifs */}
