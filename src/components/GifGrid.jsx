b45f4d82a5f5dbc706d370cb21471a2fb8dc7749
Cesar Pineda <cesar.pineda@kodevox.com>
2025-01-21 16:06:50 -0600

    🩹 Refactor getGifs function parameter to improve clarity in GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -7,7 +7,7 @@ export const GifGrid = ({ category }) => {
     const [gifs, setGifs] = useState([]);
     
     useEffect(() => {
-        const getGifs = async ({category}) => {
+        const getGifs = async (category) => {
             const url = `https://api.giphy.com/v1/gifs/search?api_key=1Vhwzebz7ptWEc2U7znMfG8LD1E5wFMh&q=${category}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
             const resp = await fetch(url);
             const { data } = await resp.json();
