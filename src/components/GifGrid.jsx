c0abb5d2b581dc685d5d8c9412b31b050170f5b2
Cesar Pineda <cesar.pineda@kodevox.com>
2025-01-21 18:02:25 -0600

    ✨ Move GIF fetching logic to a separate helper function for better modularity in GifGrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,18 +2,13 @@ import PropTypes from 'prop-types'
 import { useEffect, useState } from 'react';
 import "./AddCategory.css";
 import { GifItem } from './GifItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
     const [gifs, setGifs] = useState([]);
     
     useEffect(() => {
-        const getGifs = async (category) => {
-            const url = `https://api.giphy.com/v1/gifs/search?api_key=1Vhwzebz7ptWEc2U7znMfG8LD1E5wFMh&q=${category}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
-            const resp = await fetch(url);
-            const { data } = await resp.json();
-            setGifs(data);
-        }
-        getGifs(category);
+        getGifs(category).then(data=> setGifs(data));
     }, []);
     
     
