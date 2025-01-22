2067be605fcca6ed35acf427c419b90dc61998a2
htmtmx <cesar.hpp96@hotmail.com>
2025-01-21 21:15:45 -0600

    🔧 Update GIPHY API key retrieval to use environment variable for improved security

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,7 @@
+import { ENV } from "../config/env";
+
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=1Vhwzebz7ptWEc2U7znMfG8LD1E5wFMh&q=${category}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ENV.GIPHY_API_KEY}&q=${category}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     return data;
